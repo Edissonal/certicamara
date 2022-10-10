6deeaa78411913c49decf407b41f3fd3853b2e7a
edisson.alonso <edisson.alonso@certicamara.col>
2022-10-10 17:46:10 -0500

    implementacion de alertas

--- src/app/modulos/ssps/ssps.component.ts
+++ src/app/modulos/ssps/ssps.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 declare var window: any;
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-ssps',
@@ -10,7 +12,7 @@ export class SspsComponent implements OnInit {
   
   sspsl:any;
   sspsh:any;
-
+  valores:'';
   constructor() { } 
 
   /*implementacion de modal listas */
@@ -26,19 +28,32 @@ export class SspsComponent implements OnInit {
 
 */
      this.sspsl.show();
+    
+     /*implementacion de alertas*/
+     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+       return new bootstrap.Tooltip(tooltipTriggerEl)
+     })
+     
   }
 
-
+/*implementacion de modal ssps*/
   cerrarmodal(){
   this.sspsl.hide();
   
   }
 
-  nextssspl(){
-    this.sspsl.hide();
-    this.sspsh.show();
-  
-  }
+/*validar los checks*/
+
+validar(){
+
+}
+
+datos(){
+console.log(this.valores);
+}
+
+}
 
   
-}
+
